e1cbb76469e33c902561d1901f5671f353255067
terofolf <nathanieljay216@gmail.com>
2025-12-16 18:39:51 -0700

    updated install icon and path

--- forge.config.js
+++ forge.config.js
@@ -9,7 +9,9 @@ module.exports = {
                 authors: "Nathan Olson and Monochromish",
                 description: "An elegant, efficient, easy-to-setup and arguably the best Last.fm discord rich presence!",
                 certificateFile: "./cert.pfx",
-                certificatePassword: process.env.CERTIFICATE_PASSWORD
+                certificatePassword: process.env.CERTIFICATE_PASSWORD,
+                iconUrl: "https://raw.githubusercontent.com/Monochromish/Last.fm-Discord-Rich-Presence/main/src/icons/logo.png",
+                setupUrl: "/src/icons/logo.ico",
             },
         },
         {
@@ -31,7 +33,7 @@ module.exports = {
       config: {
         repository: {
           owner: 'no53488',
-          name: 'Last.fm-Discord-Rich-Presence'
+          name: 'Last-fm-Discord-Rich-Presence'
         },
         prerelease: false,
         draft: true
