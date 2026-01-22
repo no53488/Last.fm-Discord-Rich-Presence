a8a07184d165d7ec4ba8bd075d90bb83217e1d21
Nathan Olson <nathan.olson@ucalgary.ca>
2026-01-21 17:31:59 -0700

    update

--- forge.config.js
+++ forge.config.js
@@ -5,30 +5,41 @@ module.exports = {
   packagerConfig: {
         win32metadata: "Nathan Olson",
         asar: true,
-      },
-    makers: [
-        {
-            name: "@electron-forge/maker-squirrel",
-            config: {
-                authors: "Nathan Olson and Monochromish",
-                description: "An elegant, efficient, easy-to-setup and arguably the best Last.fm discord rich presence!",
-                certificateFile: "./cert.pfx",
-                certificatePassword: process.env.CERTIFICATE_PASSWORD,
-                iconUrl: "https://raw.githubusercontent.com/Monochromish/Last.fm-Discord-Rich-Presence/main/src/icons/logo.png",
-                setupUrl: "/src/icons/logo.ico",
-            },
+        name: "last-fm-rich-presence",
+        executableName: "LastFM-RPC"
+  },
+  makers: [
+    {
+        name: "@electron-forge/maker-squirrel",
+        config: {
+            authors: "Nathan Olson and Monochromish",
+            description: "An elegant, efficient, easy-to-setup and arguably the best Last.fm discord rich presence!",
+            certificateFile: "./cert.pfx",
+            certificatePassword: process.env.CERTIFICATE_PASSWORD,
+            iconUrl: "https://raw.githubusercontent.com/Monochromish/Last.fm-Discord-Rich-Presence/main/src/icons/logo.png",
+            setupUrl: "/src/icons/logo.ico",
         },
-        {
-          name: '@electron-forge/maker-flatpak',
-          config: {
-            options: {
-              categories: ['Video'],
-              mimeType: ['video/h264']
-            }
-          }
+    },
+    /*{
+      name: '@electron-forge/maker-flatpak',
+      config: {
+        options: {
+          categories: ['Video'],
+          mimeType: ['video/h264']
         }
-    ],
-    publishers: [
+      }
+    },*/
+    /* {
+      name: '@electron-forge/maker-deb',
+      config: {
+        options: {
+          maintainer: 'Nathan Olson',
+          homepage: 'https://felid.work',
+        }
+      }
+    }*/
+  ],
+  publishers: [
     {
       name: '@electron-forge/publisher-github',
       config: {
